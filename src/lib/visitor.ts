5b374cef0728f3c3d4b99435edd91ad5dcdeaa02
Sergey Korolev <skorolev@tjc-group.com>
2021-05-10 15:24:37 +0100

    minor issues in visitor

--- src/lib/visitor.ts
+++ src/lib/visitor.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Token } from "@tjc-group/odata-v2-parser/lib/lexer";
 // import { Literal as ODataLiteral } from "odata-v4-literal";
 import { Literal } from "odata-v4-literal";
 import { SQLLiteral as ODataSQLLiteral, SQLLang, Visitor } from "odata-v4-sql/lib/visitor";
 import { SqlOptions } from "./index";
 
-const binaryMask = /^(binary|X)\'([0-9a-fA-F]*)\'/;
+const binaryMask = /^(binary|X)'([0-9a-fA-F]*)'/;
 
 // class Literal extends ODataLiteral {
 // 	static convert(type: string, value: string): any {
@@ -25,9 +27,9 @@ class SQLLiteral extends ODataSQLLiteral {
 	}
 	'Edm.Binary'(value: string): any {
 		if (binaryMask.test(value)) {
-			return "UNHEX(\'" + value.match(binaryMask)[2] + "\')";
+			return "UNHEX('" + value.match(binaryMask)[2] + "')";
 		} else {
-			return "UNHEX(\'" + Buffer.from(value, "base64").toString("hex") + "\')";
+			return "UNHEX('" + Buffer.from(value, "base64").toString("hex") + "')";
 		}
 	}
 }
@@ -43,8 +45,8 @@ export class MySQLVisitor extends Visitor {
 		this.type = SQLLang.MySql;
 	}
 
-	from(table: string) {
-		let alias = (<any>this.options).alias;
+	from(table: string): string {
+		const alias = (<any>this.options).alias;
 		let sql = alias ?
 			`SELECT ${this.select} FROM \`${table}\` AS ${alias} WHERE ${this.where} ORDER BY ${this.orderby}` :
 			`SELECT ${this.select} FROM \`${table}\` WHERE ${this.where} ORDER BY ${this.orderby}`;
@@ -53,13 +55,13 @@ export class MySQLVisitor extends Visitor {
 		return sql;
 	}
 
-	protected VisitFormat(node: Token, context: any) {
+	protected VisitFormat(node: Token, context: any): void {
 		this.format = node.value.format;
 	}
 
-	protected VisitExpand(node: Token, context: any) {
+	protected VisitExpand(node: Token, context: any): void {
 		node.value.items.forEach((item) => {
-			let expandPath = item.value.path.raw;
+			const expandPath = item.value.path.raw;
 			let visitor = this.includes.filter(v => v.navigationProperty == expandPath)[0];
 			if (!visitor) {
 				visitor = new MySQLVisitor(this.options);
@@ -71,29 +73,29 @@ export class MySQLVisitor extends Visitor {
 		});
 	}
 
-	protected VisitSelectItem(node: Token, context: any) {
-		let item = node.raw.replace(/\//g, '.');
-		let alias = (<any>this.options).alias;
+	protected VisitSelectItem(node: Token, context: any): void {
+		const item = node.raw.replace(/\//g, '.');
+		const alias = (<any>this.options).alias;
 		this.select += alias ? `${alias}.\`${item}\`` : `\`${item}\``;
 	}
 
-	protected VisitODataIdentifier(node: Token, context: any) {
-		let alias = (<any>this.options).alias;
+	protected VisitODataIdentifier(node: Token, context: any): void {
+		const alias = (<any>this.options).alias;
 		this[context.target] += alias ? `${alias}.\`${node.value.name}\`` : `\`${node.value.name}\``;
 		context.identifier = node.value.name;
 	}
 
-	protected VisitEqualsExpression(node: Token, context: any) {
+	protected VisitEqualsExpression(node: Token, context: any): void {
 		this.Visit(node.value.left, context);
 		this.where += " = ";
 		this.Visit(node.value.right, context);
-		let alias = (<any>this.options).alias;
+		const alias = (<any>this.options).alias;
 
-		let identifierMask = alias ?
-			new RegExp(`\\? = ${alias}\.\\\`${context.identifier}\\\`$`) :
+		const identifierMask = alias ?
+			new RegExp(`\\? = ${alias}.\\\`${context.identifier}\\\`$`) :
 			new RegExp(`\\? = \\\`${context.identifier}\\\`$`);
 
-		let replaceStr = alias ? `${alias}.\`${context.identifier}\`  IS NULL` : `\`${context.identifier}\` IS NULL`
+		const replaceStr = alias ? `${alias}.\`${context.identifier}\`  IS NULL` : `\`${context.identifier}\` IS NULL`
 
 		if (this.options.useParameters && context.literal == null) {
 			this.where = this.where.replace(/= \?$/, "IS NULL").replace(identifierMask, replaceStr);
@@ -102,17 +104,17 @@ export class MySQLVisitor extends Visitor {
 		}
 	}
 
-	protected VisitNotEqualsExpression(node: Token, context: any) {
+	protected VisitNotEqualsExpression(node: Token, context: any): void {
 		this.Visit(node.value.left, context);
 		this.where += " <> ";
 		this.Visit(node.value.right, context);
 
-		let alias = (<any>this.options).alias;
-		let identifierMask = alias ?
-			new RegExp(`\\? = ${alias}\.\\\`${context.identifier}\\\`$`) :
+		const alias = (<any>this.options).alias;
+		const identifierMask = alias ?
+			new RegExp(`\\? = ${alias}.\\\`${context.identifier}\\\`$`) :
 			new RegExp(`\\? = \\\`${context.identifier}\\\`$`);
 
-		let replaceStr = alias ? `${alias}.\`${context.identifier}\`  IS NULL` : `\`${context.identifier}\` IS NOT NULL`
+		const replaceStr = alias ? `${alias}.\`${context.identifier}\`  IS NULL` : `\`${context.identifier}\` IS NOT NULL`
 
 		if (this.options.useParameters && context.literal == null) {
 			this.where = this.where.replace(/<> \?$/, "IS NOT NULL").replace(identifierMask, replaceStr);
@@ -121,23 +123,23 @@ export class MySQLVisitor extends Visitor {
 		}
 	}
 
-	protected VisitLiteral(node: Token, context: any) {
+	protected VisitLiteral(node: Token, context: any): void {
 		if (this.options.useParameters) {
-			let value = Literal.convert(node.value, node.raw);
+			const value = Literal.convert(node.value, node.raw);
 			context.literal = value;
 			this.parameters.push(value);
 			this.where += "?";
 		} else this.where += (context.literal = SQLLiteral.convert(node.value, node.raw));
 	}
 
-	protected VisitMethodCallExpression(node: Token, context: any) {
-		var method = node.value.method;
-		var params = node.value.parameters || [];
+	protected VisitMethodCallExpression(node: Token, context: any): void {
+		const method = node.value.method;
+		const params = node.value.parameters || [];
 		switch (method) {
 			case "contains":
 				this.Visit(params[0], context);
 				if (this.options.useParameters) {
-					let value = Literal.convert(params[1].value, params[1].raw);
+					const value = Literal.convert(params[1].value, params[1].raw);
 					this.parameters.push(`%${value}%`);
 					this.where += " like ?";
 				} else this.where += ` like '%${SQLLiteral.convert(params[1].value, params[1].raw).slice(1, -1)}%'`;
@@ -145,7 +147,7 @@ export class MySQLVisitor extends Visitor {
 			case "endswith":
 				this.Visit(params[0], context);
 				if (this.options.useParameters) {
-					let value = Literal.convert(params[1].value, params[1].raw);
+					const value = Literal.convert(params[1].value, params[1].raw);
 					this.parameters.push(`%${value}`);
 					this.where += " like ?";
 				} else this.where += ` like '%${SQLLiteral.convert(params[1].value, params[1].raw).slice(1, -1)}'`;
@@ -153,7 +155,7 @@ export class MySQLVisitor extends Visitor {
 			case "startswith":
 				this.Visit(params[0], context);
 				if (this.options.useParameters) {
-					let value = Literal.convert(params[1].value, params[1].raw);
+					const value = Literal.convert(params[1].value, params[1].raw);
 					this.parameters.push(`${value}%`);
 					this.where += " like ?";
 				} else this.where += ` like '${SQLLiteral.convert(params[1].value, params[1].raw).slice(1, -1)}%'`;
@@ -178,7 +180,7 @@ export class MySQLVisitor extends Visitor {
 				this.Visit(params[1], context);
 				if (params[0].value == "Edm.String") {
 					if (this.options.useParameters) {
-						let value = Literal.convert(params[0].value, params[0].raw);
+						const value = Literal.convert(params[0].value, params[0].raw);
 						this.parameters.push(`%${value}%`);
 						this.where += " like ?";
 						// this.where += ` like \$${this.parameters.length}`;
